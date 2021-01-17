ab2996b9ff565ab9a6ef095540ea0f37ec499446
Gabriel Batista <gabriel.goncalves@libbs.com.br>
2021-01-17 20:55:33 -0300

    reconfigurando acesso ao banco de dados por variavel de ambiente

--- src/config.js
+++ src/config.js
@@ -2,6 +2,15 @@ import knex from 'knex';
 
 export const client = knex({
   client: 'pg',
-  version: '7.2',
-  connection: process.env.DATABASE_URL
+  version: '12',
+  connection:{
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DB,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
 });
