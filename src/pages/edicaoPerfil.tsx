890fc5b669b17c0982308d670d5147c9fa9b6494
Guids26 <guilhermedantas006@gmail.com>
2021-01-31 15:01:03 -0300

    scroll view na edicaoPerfil

--- src/pages/edicaoPerfil.tsx
+++ src/pages/edicaoPerfil.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Platform, Text, View, Image, TextInput, TouchableOpacity, } from 'react-native';
+import { StyleSheet, Platform, Text, View, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Cabecalho from '../components/cabecalho';
 import { SafeAreaView } from 'react-navigation';
@@ -10,21 +10,23 @@ export default function EdicaoPerfil() {
         <SafeAreaView style={styles.droidSafeArea}>
             <Cabecalho />
             <View style={styles.conteudo}>
-                <TouchableOpacity>
-                    <Image source={require('../imgs/perfil.png')} style={styles.fotoPerfil} />
-                </TouchableOpacity>
-                <TextInput placeholder={"Nome"} value={"Ednaldo Pereira"} style={styles.input} />
-                <TextInput placeholder={"Nome"} value={"ednaldo.chance@yahoo.com.br"} style={styles.input} />
-                <TextInput placeholder={"Nome"} value={"17/10/1967"} style={styles.input} />
-                <TextInput placeholder={"Nome"} value={"(11) 4002-8922"} style={styles.input} />
-                <TouchableOpacity>
-                    <Text style={styles.link}>
-                        Redefinir senha
+                <ScrollView>
+                    <TouchableOpacity>
+                        <Image source={require('../imgs/perfil.png')} style={styles.fotoPerfil} />
+                    </TouchableOpacity>
+                    <TextInput placeholder={"Nome"} value={"Ednaldo Pereira"} style={styles.input} />
+                    <TextInput placeholder={"Nome"} value={"ednaldo.chance@yahoo.com.br"} style={styles.input} />
+                    <TextInput placeholder={"Nome"} value={"17/10/1967"} style={styles.input} />
+                    <TextInput placeholder={"Nome"} value={"(11) 4002-8922"} style={styles.input} />
+                    <TouchableOpacity>
+                        <Text style={styles.link}>
+                            Redefinir senha
                     </Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.botaoSalvar} >
-                    <Text style={styles.botaoSalvarTexto}>Salvar</Text>
-                </TouchableOpacity>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.botaoSalvar} >
+                        <Text style={styles.botaoSalvarTexto}>Salvar</Text>
+                    </TouchableOpacity>
+                </ScrollView>
             </View>
         </SafeAreaView>
     )
@@ -41,6 +43,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flexDirection: 'column',
         height: '100%',
+        width: '100%',
     },
     fotoPerfil: {
         width: 150,
@@ -63,23 +66,23 @@ const styles = StyleSheet.create({
         backgroundColor: '#3385FF',
         width: 144,
         height: 50,
-    
+
         padding: 10,
         borderRadius: 40,
         marginTop: 30,
-    
+
         flexDirection: 'column',
         alignContent: 'center',
         justifyContent: 'center',
-      },
-      botaoSalvarTexto: {
+    },
+    botaoSalvarTexto: {
         textAlign: 'center',
         color: '#FFFFFF',
         fontSize: 24,
-      },
-      link: {
-          marginTop: 30,
-          textDecorationLine: 'underline',
-          fontSize: 20,
-      }
+    },
+    link: {
+        marginTop: 30,
+        textDecorationLine: 'underline',
+        fontSize: 20,
+    }
 });
