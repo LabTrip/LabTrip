9d2b9289ccf95ecca28e65e1a0a3ae732a104aa2
Gabriel Batista <biel0828@hotmail.com>
2021-01-31 13:08:06 -0300

    Aceitando modificações do LoginCrontoller

--- src/login/LoginController.js
+++ src/login/LoginController.js
@@ -20,20 +20,12 @@ export default class LoginController {
 
   async autentica(req, res){
     const{email, senha} = req.body;
-<<<<<<< HEAD
-    
-    const usuario = await this.loginRepository.buscaPorEmail(email);
-    
-    if(usuario.senha == sha256(senha).toString()){
-      console.log("verificou senha")
-=======
 
     const usuario = await new Login(this.usuarioRepository.getByEmail(email));
 
     senha = sha256(senha).toString();
 
     if(usuario.senha == senha){
->>>>>>> c6ae6fd66b285d0faf3f3cd424bea4d9aad81673
       const token = jwt.sign(usuario.email, process.env.SECRET);
       const usuarioAuth = new Login(usuario.id, usuario.nome, usuario.email, usuario.foto, usuario.telefone, usuario.perfilId, token)
       return res.status(200).json(loginViewModel(usuarioAuth)); 
