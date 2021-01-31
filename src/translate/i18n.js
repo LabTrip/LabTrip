bcc953ddffd1b34759016626cc0a4be9b2470e1b
renan tiengo <renan.l.tiengo@gmail.com>
2021-01-31 17:12:20 -0300

    Estrutura para Internacionalização do sistema

---
+++ src/translate/i18n.js
@@ -0,0 +1,17 @@
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { LayoutAnimation } from 'react-native'
+import {messages} from './languages'
+
+i18n
+    .use(LanguageDetector)
+    .init ({
+        debug: false,
+        defaultNS: ['translations'],
+        fallbackLng: 'pt',
+        ns: ['translations'],
+        resources: messages
+
+    })
+
+export {i18n}
---
+++ src/translate/languages/en.js
@@ -0,0 +1,28 @@
+const messages  = {
+    en: {
+        translations: {
+            login: {
+                titulo: 'Hello',
+                saudacao: 'Welcome to Labtrip.'
+            },
+            redefinirSenha:{
+                titulo: 'Let\'s reset your password',
+                inserirCodigo:'Enter the code which you received',
+                inserirEmail: 'Enter your e-mail',
+                InserirNovaSenha: 'Enter your new password and confirm it',
+                sucesso: 'your password has been reset successfully'
+            },
+            botoes:{
+                salvar: 'Save',
+                deletar:'Delete',
+                editar:'Edit',
+                sim:'Yes',
+                nao:'No'
+            }             
+            
+
+        }
+    }
+}
+
+export{messages}
