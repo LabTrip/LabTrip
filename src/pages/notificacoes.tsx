42e7e3915f563bc84a5a441da3e3b6d419055788
Guilherme Dantas <guilhermedantas006@gmail.com>
2021-02-02 18:33:25 -0300

    trocando rotas do menu principal, add novas telas

---
+++ src/pages/notificacoes.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Notificacoes() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notificações!</Text>
+    </View>
+  );
+}
