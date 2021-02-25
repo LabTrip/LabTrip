9743478c7c31c89b3ba80f71076c86805cac09ff
Guilherme Dantas <guilhermedantas006@gmail.com>
2021-02-25 16:06:29 -0300

    alterando nome da função do MenuLaboratorio

--- src/navigations/menu/menuLaboratorio.tsx
+++ src/navigations/menu/menuLaboratorio.tsx
@@ -1,15 +1,15 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import React from 'react'
 import ListaEditarViagens from '../../pages/laboratorio/listaEditarViagens';
-import LaboratorioCadastro from './menuLaboratorioCadastro';
+import MenuLaboratorioCadastro from './menuLaboratorioCadastro';
 
 const Tab = createMaterialTopTabNavigator();
 
-export default function Laboratorio() {
+export default function MenuLaboratorio() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Viagens" component={ListaEditarViagens} />
-      <Tab.Screen name="Cadastros" component={LaboratorioCadastro} />
+      <Tab.Screen name="Cadastros" component={MenuLaboratorioCadastro} />
     </Tab.Navigator>
   );
 }
