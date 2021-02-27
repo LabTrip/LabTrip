901400b7cbfb34d5ef759d0d47a3d69c3129898e
Matheus Reis <matheus.reisluz@gmail.com>
2021-02-27 00:23:39 -0300

    inicio estilizacao criar agencia e usuario

--- src/pages/laboratorio/criarAgencia.tsx
+++ src/pages/laboratorio/criarAgencia.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 export default function CriarAgencia() {
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Criar Agencia</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+}
+});
--- src/pages/laboratorio/criarUsuario.tsx
+++ src/pages/laboratorio/criarUsuario.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 export default function CriarUsuario() {
   return (
-    <View>
+    <View style={styles.container}> 
       <Text>Criar Usuario</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+}
+});
