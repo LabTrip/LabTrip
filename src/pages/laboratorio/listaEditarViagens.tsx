781763b8a248361770f931cbd34f48a1e6b8cf48
Guids26 <guilhermedantas006@gmail.com>
2021-02-28 19:31:55 -0300

    utilizando FlatList nas viagens do laboratorio

--- src/pages/laboratorio/listaEditarViagens.tsx
+++ src/pages/laboratorio/listaEditarViagens.tsx
@@ -1,43 +1,86 @@
 import React from 'react';
-import { StyleSheet, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, View, Image, TouchableOpacity, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import BarraPesquisa from '../../components/barraPesquisa';
 import CardViagem from '../../components/cardViagem';
+import ScrollViewFlat from '../../components/scrollViewFlat';
 
 
 const Tab = createMaterialBottomTabNavigator();
 
 export default function ListaEditarViagens() {
   const navigation = useNavigation();
+  let viagensData = [
+    {
+      id: 'a6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b',
+      nome: "Fim de semana em Ubatuba",
+      dataInicio: "26/02/2021",
+      dataFim: "28/02/2021",
+      local: "Ubatuba - SP",
+      status: "Agendada",
+      navigate: "EditarViagem"
+    },
+    {
+      id: 'a6b86b273ff34fce19d6b804efaaaf5a3f5747ada4eaa22f1d49c01e52ddb7875b4b',
+      nome: "Fim de semana em Ubatuba",
+      dataInicio: "26/02/2021",
+      dataFim: "28/02/2021",
+      local: "Ubatuba - SP",
+      status: "Em andamento",
+      navigate: "EditarViagem"
+    },
+    {
+      id: '4e07408562bedb8b60aaaace05c1decfe3ad16b72230967de01f640b7e4729b49fce',
+      nome: "Fim de semana em Ubatuba",
+      dataInicio: "26/02/2021",
+      dataFim: "28/02/2021",
+      local: "Ubatuba - SP",
+      status: "Concluida",
+      navigate: "EditarViagem"
+    },
+    {
+      id: '4e07408562bedb8b60ce05c1decfe3ad16b72230961fe01f640b7e4729baaa49fce',
+      nome: "Fim de semana em Ubatuba",
+      dataInicio: "26/02/2021",
+      dataFim: "28/02/2021",
+      local: "Ubatuba - SP",
+      status: "Cancelada",
+      navigate: "EditarViagem"
+    }
+  ];
+
   return (
     <View style={{ flex: 1, backgroundColor: '#fff' }}>
       <BarraPesquisa texto={'Pesquisar viagem...'} />
-      <ScrollView>
-        <View style={styles.conteudo}>
+      <ScrollViewFlat>
+        <View style={{ alignItems: 'center' }}>
           <TouchableOpacity style={styles.botaoMais} onPress={() => navigation.navigate('CriarViagem')}>
             <Image source={require('../../imgs/plus-circle.png')} />
           </TouchableOpacity>
-          <CardViagem nome="Fim de semana em Ubatuba" dataInicio="26/02/2021"
-            dataFim="28/02/2021" local="Ubatuba - SP" status="Agendada" navigate="EditarViagem" />
-          <CardViagem nome="Fim de semana em Ubatuba" dataInicio="26/02/2021"
-            dataFim="28/02/2021" local="Ubatuba - SP" status="Em andamento" navigate="EditarViagem" />
-          <CardViagem nome="Fim de semana em Ubatuba" dataInicio="26/02/2021"
-            dataFim="28/02/2021" local="Ubatuba - SP" status="Concluida" navigate="EditarViagem" />
-          <CardViagem nome="Fim de semana em Ubatuba" dataInicio="26/02/2021"
-            dataFim="28/02/2021" local="Ubatuba - SP" status="Cancelada" navigate="EditarViagem" />
         </View>
-      </ScrollView>
-    </View>
+        <View style={{ width: 2000, alignItems: 'center' }}>
+          <FlatList
+            style={{ flexGrow: 1, flex: 1 }}
+            data={viagensData}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <CardViagem nome={item.nome} dataInicio={item.dataInicio} dataFim={item.dataFim}
+                local={item.local} status={item.status} navigate={item.navigate} item={item} />
+            )}
+          />
+        </View>
+      </ScrollViewFlat>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   conteudo: {
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     flexDirection: 'column',
-    height: '100%',
   },
   botaoMais: {
     marginTop: 20,
