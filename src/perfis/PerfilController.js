65a1474e9dcb5c751f3ddf854447abc886dfc363
Gabriel Batista <biel0828@hotmail.com>
2021-02-28 18:06:23 -0300

    adicionando try catch para as rotas de perfis

--- src/perfis/PerfilController.js
+++ src/perfis/PerfilController.js
@@ -12,26 +12,38 @@ export default class PerfilController {
 
   //GET /perfis
   async buscaTodos(req, res) {
-    let perfis;
-    switch(req.acesso.tipoAcesso){
-      case 'Total':
-          perfis = await this.perfilRepository.buscaTodos();
-        break;
-      case 'Gerencial':
-          perfis = await this.perfilRepository.buscaTodos_AcessoGerencial();
-        break;
-      case 'Parcial':
-          perfis = await this.perfilRepository.buscaTodos_AcessoParcial();
-        break;
-      default:
-          return res.status(404).json({status:'404', mensagem:'Acesso restrito.'})
+    try{
+      let perfis;
+      switch(req.acesso.tipoAcesso){
+        case 'Total':
+            perfis = await this.perfilRepository.buscaTodos();
+          break;
+        case 'Gerencial':
+            perfis = await this.perfilRepository.buscaTodos_AcessoGerencial();
+          break;
+        case 'Parcial':
+            perfis = await this.perfilRepository.buscaTodos_AcessoParcial();
+          break;
+        default:
+            return res.status(404).json({status:'404', mensagem:'Acesso restrito.'})
+      }
+      
+      res.status(200).json({perfis: perfis.map(u => perfilViewModel(u))});
+    }
+    catch(e){
+      return res.status(400).json({status: '400', mensagem: 'Entrada de informações incorretas.'});
     }
     
-    res.status(200).json({perfis: perfis.map(u => perfilViewModel(u))});
   }
 
   mostra(req, res){
-    return res.status(200).json(perfilViewModel(req.perfil)); 
+    try{
+      return res.status(200).json(perfilViewModel(req.perfil));
+    }
+    catch(e){
+      return res.status(400).json({status: '400', mensagem: 'Entrada de informações incorretas.'});
+    }
+     
   }
 
 }
