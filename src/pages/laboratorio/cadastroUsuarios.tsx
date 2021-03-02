257c6c49ac3e72760262101fc83ec399b11b011a
Matheus Reis <matheus.reisluz@gmail.com>
2021-03-02 18:57:07 -0300

    teste de tabela com DataTable

--- src/pages/laboratorio/cadastroUsuarios.tsx
+++ src/pages/laboratorio/cadastroUsuarios.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, StyleSheet,TouchableOpacity, View, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { DataTable } from 'react-native-paper';
 
 export default function CadastroUsuario() {
   const navigation = useNavigation();
@@ -10,6 +11,33 @@ export default function CadastroUsuario() {
         <Image source={require('../../imgs/plus-circle.png')} />
       </TouchableOpacity>
       <Text>cadastro de usuarios</Text>
+
+      <DataTable>
+        <DataTable.Header >
+          <DataTable.Title>Nome</DataTable.Title>
+          <DataTable.Title>E-mail</DataTable.Title>
+          <DataTable.Title>Perfil</DataTable.Title>
+        </DataTable.Header>
+
+        <DataTable.Row style={styles.corpoTabela}>
+          <DataTable.Cell>Ednaldo Pereira</DataTable.Cell>
+          <DataTable.Cell>ednaldo.pereira@gmail.com</DataTable.Cell>
+          <DataTable.Cell>Proprietário</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row style={styles.corpoTabela}>
+          <DataTable.Cell>Ednaldo Agente</DataTable.Cell>
+          <DataTable.Cell>ednaldo.agente@gmail.com</DataTable.Cell>
+          <DataTable.Cell>Agente</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row style={styles.corpoTabela}>
+          <DataTable.Cell>Ednaldo Administrador</DataTable.Cell>
+          <DataTable.Cell>ednaldo.adm@gmail.com</DataTable.Cell>
+          <DataTable.Cell>Administrador</DataTable.Cell>
+        </DataTable.Row>
+      </DataTable>
+
     </View>
   );
 }
@@ -22,5 +50,15 @@ const styles = StyleSheet.create({
   },
   botaoMais: {
     margin: 20
+  },
+  cabecalhoTabela: {
+    backgroundColor: 'black',
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  corpoTabela: {
+    backgroundColor: '#EBEBEB'
   }
+
 })
