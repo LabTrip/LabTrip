5dc70e3fd0ffede9f626b54d16ef5535c993e41d
Guilherme Dantas <guilhermedantas006@gmail.com>
2021-03-03 20:52:37 -0300

    corrigindo editarPerfil para aceitar mudar inputs

--- src/pages/profile/editarPerfil.tsx
+++ src/pages/profile/editarPerfil.tsx
@@ -1,82 +1,76 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 const moment = require('moment');
 
-interface Usuario {
-    nome: string,
-    email: string,
-    telefone: string
-    dataNascimento: string
-}
 
-export default function EditarPerfil({ route }) {
-    const [nome, onChangeTextNome] = React.useState('Ednaldo Pereira');
-    const [email, onChangeTextEmail] = React.useState('ednaldo.chance@yahoo.com.br');
-    const [data, onChangeTextData] = React.useState('17/10/1967');
-    const [telefone, onChangeTextTelefone] = React.useState('(11) 4002-8922');
-    const { token } = route.params;
-    let Token;
-    let userId;
-    const [usuario, setUsuario] = useState<Usuario>()
-    const getViagens = async () => {
-        return await fetch('https://labtrip-backend.herokuapp.com/usuarios/' + userId , {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-        'Content-Type': 'application/json',
-            'x-access-token': Token
-        }
+export default function EditarPerfil() {
+    const [nome, onChangeTextNome] = useState("");
+    const [email, onChangeTextEmail] = useState("");
+    const [data, onChangeTextData] = useState("");
+    const [telefone, onChangeTextTelefone] = useState("");
+
+    let token, userId;
+
+    const getUsuario = async () => {
+        return await fetch('https://labtrip-backend.herokuapp.com/usuarios/' + userId, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'x-access-token': token
+            }
         });
     }
 
-    useEffect(() => {
+    const buscaPreencheUsuario = () => useEffect(() => {
         const request = async () => {
-        try{
-            const value = await AsyncStorage.getItem('AUTH');            
-            const user = await AsyncStorage.getItem('USER_ID');
-            //setToken(value);
-            if(value !== null){
-                Token = JSON.parse(value)
+            try {
+                const value = await AsyncStorage.getItem('AUTH');
+                const user = await AsyncStorage.getItem('USER_ID');
+                if (value !== null) {
+                    token = JSON.parse(value)
+                }
+                console.log(user)
+                if (user !== null) {
+                    userId = JSON.parse(user)
+                }
+                console.log(token)
+                const response = await getUsuario();
+                const json = await response.json();
+                if (response.status == 200) {
+                    onChangeTextNome(json.nome);
+                    onChangeTextEmail(json.email);
+                    onChangeTextData(moment(json.dataNascimento).format('DD/MM/yyyy'));
+                    onChangeTextTelefone(json.telefone);
+                }
             }
-            console.log(user)
-            if(user !== null){
-                userId = JSON.parse(user)
+            catch (e) {
+                console.log(e)
             }
-            console.log(Token)
-            const response = await getViagens();
-            const json = await response.json();
-            console.log(response.status)
-            if(response.status == 200){
-            setUsuario(json);
-            }
-        }
-        catch(e){
-            console.log(e)
-        }
         }
 
         request()
-        
+
     }, [])
 
-    return (
+    buscaPreencheUsuario();
 
+    return (
         <View style={styles.container}>
-            {console.log('caiu no profile ' + token)}
             <ScrollView>
                 <View style={styles.conteudo}>
                     <TouchableOpacity>
                         <Image source={require('../../imgs/perfil.png')} style={styles.fotoPerfil} />
                     </TouchableOpacity>
-                    <TextInput placeholder={"Nome"} value={usuario?.nome} style={styles.input}
+                    <TextInput placeholder={"Nome"} value={nome} style={styles.input}
                         onChangeText={text => onChangeTextNome(text)} />
-                    <TextInput placeholder={"Email"} value={usuario?.email} style={styles.input}
+                    <TextInput placeholder={"Email"} value={email} style={styles.input}
                         onChangeText={text => onChangeTextEmail(text)} />
-                    <TextInput placeholder={"Data de nascimento"} value={moment(usuario?.dataNascimento).format('DD/MM/yyyy')} style={styles.input}
+                    <TextInput placeholder={"Data de nascimento"} value={data} style={styles.input}
                         onChangeText={text => onChangeTextData(text)} />
-                    <TextInput placeholder={"Telefone"} value={usuario?.telefone} style={styles.input}
+                    <TextInput placeholder={"Telefone"} value={telefone} style={styles.input}
                         onChangeText={text => onChangeTextTelefone(text)} />
                     <TouchableOpacity>
                         <Text style={styles.link}>
