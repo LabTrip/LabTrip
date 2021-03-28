bff694979f0e2672f22ccc74f7a686e4a7086aeb
Guilherme Dantas <guilhermedantas006@gmail.com>
2021-03-28 17:08:18 -0300

    ajustando estilo do card de viagens

--- src/components/cardViagem.tsx
+++ src/components/cardViagem.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+let corDoCard, corBordaDoCard, status, corDoStatus;
+
 export default function CardViagem(props) {
-    let corDoCard, corBordaDoCard, status;
+
     const navigation = useNavigation();
 
     switch (props.status) {
@@ -11,37 +13,50 @@ export default function CardViagem(props) {
             corDoCard = '#FFFDD1';
             corBordaDoCard = '#F8EC12';
             status = "Em Planejamento";
+            corDoStatus = '#B7AF0B'
             break;
         case 2:
             corDoCard = '#FFFDD1';
             corBordaDoCard = '#F8EC12';
             status = "Planejado";
+            corDoStatus = '#B7AF0B'
             break;
         case 3:
             corDoCard = '#CCEEFF';
             corBordaDoCard = '#00AEFF';
             status = "Em andamento";
+            corDoStatus = '#00AEFF';
             break;
         case 5:
             corDoCard = '#CEF7E3';
             corBordaDoCard = '#0FD06F';
             status = "Concluído";
+            corDoStatus = '#0FD06F';
             break;
         default:
             corDoCard = '#F0F0F0';
             corBordaDoCard = '#787878';
             status = "Cancelado"
+            corDoStatus = '#333333';
             break;
     }
 
     return (
         <TouchableOpacity style={[styles.cardViagens,
         { backgroundColor: corDoCard, borderLeftColor: corBordaDoCard }]}
-            onPress={() => navigation.navigate(props.navigate, {viagem: props.item})}>
+            onPress={() => navigation.navigate(props.navigate, { viagem: props.item })}>
             <Text>{props.nome}</Text>
-            <Text>Início: {props.dataInicio}, Fim: {props.dataFim}</Text>
-            <Text>Local: {props.local}</Text>
-            <Text>Status: {status}</Text>
+            <Text>
+                <Text style={styles.label}>Início:</Text> {props.dataInicio}
+                <Text style={styles.label}> Fim:</Text> {props.dataFim}
+            </Text>
+            <Text>
+                <Text style={styles.label}>Local:</Text> {props.local}
+            </Text>
+            <Text>
+                <Text style={styles.label}>Status: </Text>
+                <Text style={{ color: corDoStatus }}>{status}</Text>
+            </Text>
         </TouchableOpacity>
     )
 }
@@ -54,8 +69,12 @@ const styles = StyleSheet.create({
         borderLeftWidth: 6,
         width: '85%',
         height: 143,
-        justifyContent: 'center',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
         alignItems: 'center',
     },
+    label: {
+        fontWeight: 'bold'
+    }
 });
 
